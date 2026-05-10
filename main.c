d08581c8fab586cbc6210ca8f23a2a8fe186f538
Yasoda Kunwar <yasodabitm@oic.edu.np>
2026-05-10 21:55:24 +0545

    Update main.c with new changes

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Simple Calculator\n");
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if(num2 != 0)
+                printf("Result: %.2lf\n", num1 / num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
